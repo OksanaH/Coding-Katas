773a7ad8347ecee32491dfd5c443c3b1ee1a901e
Oxie <oxieking83@gmail.com>
2018-01-07 19:53:14 +0000

    Exercise 5 completed

---
+++ CodingKataClassLibrary/EulerProject_Exercise5.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingKataClassLibrary
+{
+    //https://projecteuler.net/problem=5
+    public class SmallestMultipleFinder
+    {
+        public int[] Range { get; set; }   
+           
+
+        public SmallestMultipleFinder(int[] range)
+        {
+            this.Range = range;
+        }
+        public int FindSmallestMultiple()
+        {
+            int starting = Range[1]+1;
+            int smallestMultiple = 0;
+            while (smallestMultiple==0)
+            {
+                if (CanBeMultipliedByAllNumbersInRange(starting))
+                {
+                    smallestMultiple = starting;
+                }
+                starting++;
+            }
+            return smallestMultiple;
+        }
+
+        public bool CanBeMultipliedByAllNumbersInRange(int number)
+        {
+            bool isMultipliableByAllNumbers = false;
+            for (int i = Range[0]; i <= Range[1]; i++)
+            {
+                if (number ==2520)
+                {
+
+                }
+                if (number % i !=0)
+                {
+                    isMultipliableByAllNumbers = false;
+                    break;
+                }
+                else
+                {
+                    isMultipliableByAllNumbers = true;
+                }
+
+            }
+            return isMultipliableByAllNumbers;
+        }
+       
+    }
+    
+
+}
---
+++ CodingKataTests/EulerProject_Exercise5_Test.cs
@@ -0,0 +1,36 @@
+﻿using System;
+//using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CodingKataClassLibrary;
+using NUnit.Framework;
+
+
+namespace CodingKataTests
+{
+  
+    [TestFixture]  
+    public class EulerProject_Exercise5_Test
+    {
+        //https://projecteuler.net/problem=5
+        [TestCase(new int[] { 1,10}, 100)]
+        public void Verify_NumberIsNot_Multipliable_ByAllNumbersInRange(int[] range, int expected)
+        {
+            SmallestMultipleFinder multFinder = new SmallestMultipleFinder(range);
+            Assert.IsFalse(multFinder.CanBeMultipliedByAllNumbersInRange(expected));
+        }
+
+        [TestCase(new int[] { 1, 10 }, 2520)]
+        public void Verify_NumberIs_Multipliable_ByAllNumbersInRange(int[] range, int expected)
+        {
+            SmallestMultipleFinder multFinder = new SmallestMultipleFinder(range);
+            Assert.IsTrue(multFinder.CanBeMultipliedByAllNumbersInRange(expected));
+        }
+
+        [TestCase(new int[] { 1, 10 }, 2520)]
+        [TestCase(new int[] { 1, 20 }, 232792560)]
+        public void Verify_CanFind_SmallestMultiple(int[] range, int expected)
+        { 
+            SmallestMultipleFinder multFinder = new SmallestMultipleFinder(range);
+            Assert.AreEqual(expected, multFinder.FindSmallestMultiple());
+        }
+    }
+}
